c282407e21fb5e4f37d35ebbd935781dc7ee2def
Виталий <absolut83@yandex.ru>
2022-09-07 18:17:16 +0300

    15

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# praktikum_new_diplom
+# Продуктовый помощник
+
+## Описание
+
+«Продуктовый помощник»: это ресурс, на котором пользователи публикуют рецепты, добавляют чужие рецепты в избранное и подписываются на публикации других авторов.
+
+### Запуск проекта в Docker compose
+
+- Установите Docker.
+
+Параметры запуска описаны в файлах `docker-compose.yml` и `nginx.conf` которые находятся в директории `infra/`
+
+- Запуск docker compose:
+
+```bash
+docker-compose up -d --build
+```  
+
+- Сбор статики:
+
+```bash
+docker-compose exec backend python manage.py collectstatic --no-input
+```  
+
+- Миграции:
+
+```bash
+docker-compose exec backend python manage.py migrate
+```
+
+- Запустите процесс загрузки ингредиентов и тегов (папки дата внутри backend):
+
+```bash
+docker-compose exec backend python manage.py load_ingrs
+```
+
+```bash
+docker-compose exec backend python manage.py load_tags
+```
+
+- Создайте суперпользователя:
+
+```bash
+docker-compose exec backend python manage.py createsuperuser
+```
+
+### Шаблон наполнения .env (не включен в текущий репозиторий) расположенный по пути infra/.env
+
+```python
+DB_ENGINE=django.db.backends.postgresql 
+DB_NAME=postgres 
+POSTGRES_USER=postgres 
+POSTGRES_PASSWORD=postgres 
+DB_HOST=db 
+DB_PORT=5432 
+```
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -59,24 +59,16 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='db'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv(
-#             'DB_ENGINE',
-#             default='django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', default='db'),
-#         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-#         'HOST': os.getenv('DB_HOST', default='localhost'),
-#         'PORT': os.getenv('DB_PORT', default='5432'),
-#     }
-# }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,7 +95,6 @@ USE_I18N = True
 
 USE_TZ = False
 
-# DRF & Djoser
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
